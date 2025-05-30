873fae56f14de4bf2f0efbbb9575e52df6054ce2
ChoudhariJay <choudharijay3@gmail.com>
2025-05-30 15:09:30 +1000

    Removing Index.html

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
